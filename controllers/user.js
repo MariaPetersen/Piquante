5932d5069f72132ff2948d25a7b9e0f2568eec1f
Maria Petersen <mau.petersen@gmail.com>
2023-06-07 12:08:57 +0200

    delete user check on controller

--- controllers/user.js
+++ controllers/user.js
@@ -11,24 +11,19 @@ exports.signup = (req, res, next) => {
     const { email } = req.body
     User.findOne({ email })
         .then(user => {
-            if (user) {
-                return res.status(401).json({ error })
-            }
-            else {
-                bcrypt.hash(req.body.password, 10)
-                    .then(
-                        hash => {
-                            const newUser = new User({
-                                email: req.body.email,
-                                password: hash
-                            });
-                            newUser.save()
-                                .then(() => res.status(201).json({ message: "Utilisateur enregistré" }))
-                                .catch(error => res.status(400).json({ error }))
-                        }
-                    )
-                    .catch(error => res.status(500).json({ error }))
-            }
+            bcrypt.hash(req.body.password, 10)
+                .then(
+                    hash => {
+                        const newUser = new User({
+                            email: req.body.email,
+                            password: hash
+                        });
+                        newUser.save()
+                            .then(() => res.status(201).json({ message: "Utilisateur enregistré" }))
+                            .catch(error => res.status(400).json({ error }))
+                    }
+                )
+                .catch(error => res.status(500).json({ error }))
         }
         )
 };
